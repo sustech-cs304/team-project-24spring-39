ee840c598df47908e73984a7026b0dc828b49265
phystar <100120334+phystar@users.noreply.github.com>
2024-03-18 00:15:46 +0800

    first commit

---
+++ src/test/java/com/example/cs304/Cs304ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs304;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs304ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
