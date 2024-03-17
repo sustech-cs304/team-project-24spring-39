4b3dffff7c5527ed01d07d2344c555e3470cad37
phystar <100120334+phystar@users.noreply.github.com>
2024-03-18 01:00:59 +0800

    Configuring hot deployment

--- src/main/java/com/example/cs304/controller/HelloController.java
+++ src/main/java/com/example/cs304/controller/HelloController.java
@@ -1,2 +1,12 @@
-package com.example.cs304.controller;public class HelloController {
+package com.example.cs304.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
