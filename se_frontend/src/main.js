b5a42be8d434dc86889dd34083e93600824384bc
xiaozhangmoloxi12345 <12112249@mail.sustech.edu.cn>
2024-04-12 23:25:22 +0800

    添加登录滑动验证码

--- se_frontend/src/main.js
+++ se_frontend/src/main.js
@@ -3,4 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+
+//滑动验证码
+import MakeitCaptcha from "makeit-captcha";
+import "makeit-captcha/dist/captcha.min.css";
+app.use(MakeitCaptcha);
+app.use(store).use(router).mount("#app");
