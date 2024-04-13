d20d5cf3083f1ab206af05e926ecd30cce124f36
moloxi12345 <12112203@mail.sustech.edu.cn>
2024-04-13 16:40:05 +0800

    安装element图标

---
+++ se_frontend/src/layout/components/Header/component/ExpandToggle.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Expand } from "@element-plus/icons-vue";
+</script>
+
+<template>
+  <el-tooltip
+    class="box-item"
+    effect="dark"
+    content="Right Center prompts info"
+    placement="right"
+  >
+    <el-icon><Expand /></el-icon>
+  </el-tooltip>
+</template>
+
+<style scoped lang="scss"></style>
--- se_frontend/src/main.js
+++ se_frontend/src/main.js
@@ -2,5 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 引入ElementPlus的图标
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+
+// 注册ElementPlus的图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.use(store).use(router).mount("#app");
