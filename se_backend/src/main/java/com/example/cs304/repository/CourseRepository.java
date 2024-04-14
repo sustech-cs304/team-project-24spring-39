64a0f275371c7435d8b5a6f9e5cba0745dc5697a
CassperW <wangyj25@outlook.com>
2024-04-15 00:16:16 +0800

    update CourseController and CourseRepository

--- se_backend/src/main/java/com/example/cs304/repository/CourseRepository.java
+++ se_backend/src/main/java/com/example/cs304/repository/CourseRepository.java
@@ -1,5 +1,6 @@
 package com.example.cs304.repository;
 
+import com.example.cs304.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Integer> {
