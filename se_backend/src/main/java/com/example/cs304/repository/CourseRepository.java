f135533be25962d93cca9cc769a300bcd1eae1c0
phystar <100120334+phystar@users.noreply.github.com>
2024-04-15 20:53:36 +0800

    修正了获取所有课程信息的api，现在能获取到教授的信息，但格式还要处理

--- se_backend/src/main/java/com/example/cs304/repository/CourseRepository.java
+++ se_backend/src/main/java/com/example/cs304/repository/CourseRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CourseRepository extends JpaRepository<Course, Integer> {
-@EntityGraph(attributePaths = {"professors"})
-List<Course> findAll();
+
+    @Query(value = "SELECT c.id, c.name, c.CID, c.type, c.department, c.credit, c.hours, c.capacity, c.selected, c.location, c.time, p.PID, p.name AS professor_name " +
+            "FROM course c " +
+            "LEFT JOIN course_professor cp ON c.CID = cp.course_id " +
+            "LEFT JOIN professor p ON cp.professor_id = p.PID", nativeQuery = true)
+    List<Map<String,Object>> findAllCoursesProfessors();
 }
