a31efe0750ea286f7bc9eb30ca9d70ec50c18393
CassperW <wangyj25@outlook.com>
2024-04-15 22:19:06 +0800

    update search course of SID API

--- se_backend/src/main/java/com/example/cs304/controller/StudentController.java
+++ se_backend/src/main/java/com/example/cs304/controller/StudentController.java
@@ -1,16 +1,28 @@
 package com.example.cs304.controller;
 
+import com.example.cs304.repository.StudentRepository;
 import com.example.cs304.service.StudentService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
     private final StudentService studentService;
+    private final StudentRepository studentRepository;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, StudentRepository studentRepository) {
         this.studentService = studentService;
+        this.studentRepository = studentRepository;
     }
 
+    @GetMapping("/{SID}")
+    public ResponseEntity<List<Map<String, Object>>> getStudents(@PathVariable("SID") String SID) {
+        List<Map<String, Object>> courses = studentRepository.findCourses(SID);
+        return ResponseEntity.ok(courses);
+    }
 }
