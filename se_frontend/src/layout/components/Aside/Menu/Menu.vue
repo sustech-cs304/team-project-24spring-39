3da19f2e5c483715131d0c8f76b00041e00cbeb1
moloxi12345 <12112203@mail.sustech.edu.cn>
2024-05-27 21:12:51 +0800

    fix

--- se_frontend/src/layout/components/Aside/Menu/Menu.vue
+++ se_frontend/src/layout/components/Aside/Menu/Menu.vue
@@ -32,12 +32,12 @@ const defaultActive = computed(() => {
 const menuList = computed(() => store.state.userStore.menuList);
 // const sid = computed(() => store.state.userStore.userInfo.sid);
 async function handleSelect(index) {
-  console.log(index);
+  // console.log(index);
   if (index == "/evaluation") {
     console.log("enter handel select976");
     await store.dispatch("fetchCourses", "00000001");
   }
-  console.log("menuList: ", menuList);
+  // console.log("menuList: ", menuList);
 }
 </script>
 
